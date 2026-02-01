d585184a769b52f4d2dcfc6c582cb30c12d96c48
Zach Ferretti <zf66728@gmail.com>
2026-01-31 19:26:42 -0700

    Leverages Shada for NVIM environment variables managing state persistence of background toggling.

--- lua/invert/init.lua
+++ lua/invert/init.lua
@@ -3,11 +3,24 @@ local M = {}
 -- Default Options
 M.options = {
 	keymap = "<leader>t",
+	persist = true,
 }
+
+-- Restore persisted background after shada is loaded
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		if vim.g.INVERT_BACKGROUND then
+			vim.o.background = vim.g.INVERT_BACKGROUND
+		end
+	end,
+})
 
 -- Simple function to toggle the background of a users session.
 function M.toggleBackground()
 	vim.o.background = vim.o.background == "dark" and "light" or "dark"
+	if M.options.persist then
+		vim.g.INVERT_BACKGROUND = vim.o.background
+	end
 end
 
 function M.setup(opts)
