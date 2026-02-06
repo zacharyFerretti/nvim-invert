caceee98d28a77ad6027b23cc1213cc168c921f2
Zach Ferretti <zf66728@gmail.com>
2026-02-06 11:12:49 -0700

    Ensures that for Colorschemes with multiple variants (i.e. Cattpuccin) when togggling the user is brought back to the previous color scheme not just the default light or dark variant.

--- lua/invert/init.lua
+++ lua/invert/init.lua
@@ -6,20 +6,44 @@ M.options = {
 	persist = true,
 }
 
--- Restore persisted background after shada is loaded
+-- Restore persisted background and colorscheme after shada is loaded
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		if vim.g.INVERT_BACKGROUND then
 			vim.o.background = vim.g.INVERT_BACKGROUND
+			local scheme = vim.g.INVERT_BACKGROUND == "dark"
+				and vim.g.INVERT_DARK_COLORSCHEME
+				or vim.g.INVERT_LIGHT_COLORSCHEME
+			if scheme then
+				vim.cmd("colorscheme " .. scheme)
+			end
 		end
 	end,
 })
 
--- Simple function to toggle the background of a users session.
+-- Toggle the background and colorscheme of a users session.
 function M.toggleBackground()
+	-- Save the current colorscheme for the side we're leaving
+	if M.options.persist then
+		local current_scheme = vim.g.colors_name
+		if vim.o.background == "dark" then
+			vim.g.INVERT_DARK_COLORSCHEME = current_scheme
+		else
+			vim.g.INVERT_LIGHT_COLORSCHEME = current_scheme
+		end
+	end
+
 	vim.o.background = vim.o.background == "dark" and "light" or "dark"
+
 	if M.options.persist then
 		vim.g.INVERT_BACKGROUND = vim.o.background
+		-- Restore the colorscheme for the side we're switching to
+		local target_scheme = vim.o.background == "dark"
+			and vim.g.INVERT_DARK_COLORSCHEME
+			or vim.g.INVERT_LIGHT_COLORSCHEME
+		if target_scheme then
+			vim.cmd("colorscheme " .. target_scheme)
+		end
 	end
 end
 
